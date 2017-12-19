52fadb7c1e46ad9b56e2298db069f7ea2262ef4a
Fangge Jin <fjin@localhost.localdomain>
2017-12-19 11:06:36 +0800

    Update graphics part for rhel7.5-RHV-default.xml and the related setup file

--- setup/rhel7.5-RHV-default.sh
+++ setup/rhel7.5-RHV-default.sh
@@ -6,3 +6,8 @@ interface=$1
 
 virsh iface-bridge $interface ovirtmgmt --no-stp
 
+# Setup spice tls env
+
+# Create network "vdsm-ovirtmgmt" with public ip range on host
+# This network is used for spice graphic
+
